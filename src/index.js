039b6dabf2509318c9c507d50b52998032a68075
PawelMajewskiPolska <114810121+PawelMajewskiPolska@users.noreply.github.com>
2023-04-20 01:35:25 +0200

    with pagination

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import getAllPictures from './fetchPictures';
 import renderPictures from './renderPictures';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
-/* const request = axios.create({
-  baseURL: 'https://pixabay.com/api',
-}); */
+import 'tui-pagination/dist/tui-pagination.css';
+import Pagination from 'tui-pagination';
 
 const qs = s => document.querySelector(s);
 
@@ -22,17 +20,35 @@ const displayGallery = qs('.gallery');
 const buttonLoadMore = qs('.load-more');
 
 buttonLoadMore.classList.add('is-hidden');
+
 let page;
 const perPage = 40;
+
+const updateSessionStorage = () => {
+  sessionStorage.removeItem('totalAmountofPhotos');
+};
 
 const getPhotos = async e => {
   try {
     e.preventDefault();
+
     displayGallery.innerHTML = '';
     page = 1;
 
     const pictures = await getAllPictures(perPage, page);
     const { totalHits: totalAmountofPhotos, hits: allPhotos } = pictures;
+
+    (async () =>
+      await localStorage.setItem(
+        'totalAmountofPhotos',
+        JSON.stringify(`${totalAmountofPhotos}`)
+      ))();
+    let seat = JSON.parse(localStorage.getItem('totalAmountofPhotos'));
+
+    /* 
+        pobieranie i ustawianie wartości totalPages do sessionStorage */
+
+    sessionStorage.setItem('totalAmountofPhotos', `${totalAmountofPhotos}`);
 
     if (totalAmountofPhotos === 0) {
       Notiflix.Notify.info(
@@ -42,23 +58,60 @@ const getPhotos = async e => {
       Notiflix.Notify.info(`Hooray! We found ${totalAmountofPhotos} images.`);
     }
 
-    /*   const { hits } = data; */
-
     renderPictures(allPhotos);
     const limit = totalAmountofPhotos / perPage;
     if (limit > 1) {
-      buttonLoadMore.classList.remove('is-hidden');
+      const container = document.getElementById('pagination');
+      const options = {
+        totalItems:
+          totalAmountofPhotos /* wartość totalPages z sessionStorage */,
+        itemsPerPage: perPage,
+        visiblePages: 10,
+        page: 1,
+        centerAlign: false,
+        firstItemClassName: 'tui-first-child',
+        lastItemClassName: 'tui-last-child',
+        template: {
+          page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+          currentPage:
+            '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+          moveButton:
+            '<a href="#" class="tui-page-btn tui-{{type}}">' +
+            '<span class="tui-ico-{{type}}">{{type}}</span>' +
+            '</a>',
+          disabledMoveButton:
+            '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+            '<span class="tui-ico-{{type}}">{{type}}</span>' +
+            '</span>',
+          moreButton:
+            '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+            '<span class="tui-ico-ellip">...</span>' +
+            '</a>',
+        },
+      };
+      const pagination = new Pagination('pagination', options);
+      pagination.on('afterMove', getMorePictures);
     }
+
+    const name = qs('input');
+    name.value = '';
   } catch {
     console.log('error');
   }
 };
 
+/* pobieranie wartości totalPages z sessionStorage */
+
 form.addEventListener('submit', getPhotos);
+form.addEventListener('submit', updateSessionStorage);
 
 const getMorePictures = async () => {
+  displayGallery.innerHTML = '';
+  const currentPage = document.querySelector('.tui-is-selected');
+  const currentPageToDisplay = currentPage.textContent;
+
   try {
-    page++;
+    page = currentPageToDisplay;
     const pictures = await getAllPictures(perPage, page);
     const { totalHits: totalAmountofPhotos, hits: allPhotos } = pictures;
     const limit = totalAmountofPhotos / perPage;
@@ -69,7 +122,7 @@ const getMorePictures = async () => {
       Notiflix.Notify.info(
         `We're sorry, but you've reached the end of search results.`
       );
-      buttonLoadMore.classList.add('is-hidden');
+
       const name = qs('input');
       name.value = '';
     }
@@ -77,5 +130,3 @@ const getMorePictures = async () => {
     console.log('error');
   }
 };
-
-buttonLoadMore.addEventListener('click', getMorePictures);
--- src/renderPictures.js
+++ src/renderPictures.js
@@ -1,5 +1,7 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'tui-pagination/dist/tui-pagination.css';
+import Pagination from 'tui-pagination';
 
 const qs = s => document.querySelector(s);
 
@@ -10,6 +12,51 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captiondelay: 250,
 });
+
+/* const container = document.getElementById('pagination');
+
+const options = {
+  totalItems: 500,
+  itemsPerPage: 10,
+  visiblePages: 10,
+  page: 1,
+  centerAlign: false,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
+};
+
+const pagination = new Pagination('pagination', options);
+
+pagination.on('afterMove', event => {
+  const currentPage = event.page;
+  console.log(currentPage);
+}); */
+
+/* pagination.on('beforeMove', event => {
+  const currentPage = event.page;
+
+  if (currentPage === 10) {
+    return false;
+    // return true;
+  }
+}); */
 
 const renderPictures = allPhotos => {
   const markup = allPhotos
